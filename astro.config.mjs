8fa2d0de78f0192e87043ae1ba329bc76d6bfb71
bertrand <bertranddilly@yahoo.fr>
2023-12-22 14:08:33 +0100

    auth0

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,23 +2,11 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import { loadEnv } from 'vite';
 
-import auth from "auth-astro";
-import Google from "@auth/core/providers/google"
-
 const env = loadEnv('production', process.cwd(), '');
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [auth(
-      {
-        providers: [
-          Google({
-            clientId: env.GOOGLE_ID,
-            clientSecret: env.GOOGLE_SECRET
-          })
-        ]
-      }
-  )],
+  integrations: [],
   output: "server",
   adapter: netlify()
 });
